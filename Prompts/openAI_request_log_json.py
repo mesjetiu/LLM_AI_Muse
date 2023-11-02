9893c7dae7bdf5c309b2314d66f349ac345ab017
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2023-11-02 14:38:31 +0100

    script con número ilimitado de iteraciones. Revisar

--- Prompts/openAI_request_log_json.py
+++ Prompts/openAI_request_log_json.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import json
+import datetime
 from apikey import API_KEY  # Importa la clave desde apikey.py
 
 openai.api_key = API_KEY
@@ -20,6 +21,7 @@ user_message = "hola, qué tal?"
 
 
 def make_query(system_message, user_message, model="gpt-3.5-turbo-16k-0613", temperature=1, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0):
+    print(f"Enviando consulta: {system_message} / {user_message}")
     messages = [
         {
             "role": "system",
@@ -40,43 +42,48 @@ def make_query(system_message, user_message, model="gpt-3.5-turbo-16k-0613", tem
         "presence_penalty": presence_penalty
     }
     response = openai.ChatCompletion.create(**request_params)
-    # Devuelve la respuesta completa y los parámetros de la solicitud
+    print("Respuesta recibida")
     return response, request_params
 
 
-# Inicializar una lista vacía para almacenar las entradas de log
-log_entries = []
+# Definir cuántas veces ejecutar todo el script
+num_iterations = 2
 
-# Iterar a través de los mensajes del sistema y realizar una consulta para cada uno
-for system_message in system_messages:
-    response, request_params = make_query(system_message, user_message)
-    response_content = response['choices'][0]['message']['content']
+for i in range(num_iterations):
+    print(f"Iteración {i + 1} de {num_iterations}")
 
-    # Crear un objeto de log para la consulta y respuesta actual
-    log_entry = {
-        "request": request_params,
-        "response": response['choices'][0]
-    }
+    # Obtener la fecha y hora actual para incluirlas en el nombre del archivo de log
+    current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"chat_log_{current_datetime}_iter{i + 1}.json"
 
-    # Añadir la entrada de log a la lista
-    log_entries.append(log_entry)
+    # Inicializar una lista vacía para almacenar las entradas de log
+    log_entries = []
 
-    # Actualizar user_message para la próxima iteración
-    user_message = response_content
+    # Iterar a través de los mensajes del sistema y realizar una consulta para cada uno
+    for step, system_message in enumerate(system_messages, start=1):
+        response, request_params = make_query(system_message, user_message)
+        response_content = response['choices'][0]['message']['content']
 
-# Nombre del archivo en el que se guardarán los registros
-filename = "chat_log.json"
+        # Crear un objeto de log para la consulta y respuesta actual
+        log_entry = {
+            "iteration": i + 1,
+            "group": selected_group_key,
+            "step": step,
+            "request": request_params,
+            "response": response['choices'][0]
+        }
 
-# Leer el archivo existente y deserializar el contenido a una lista
-try:
-    with open(filename, 'r') as file:
-        existing_log_entries = json.load(file)
-except FileNotFoundError:
-    existing_log_entries = []  # Si el archivo no existe, iniciar una lista vacía
+        # Añadir la entrada de log a la lista
+        log_entries.append(log_entry)
 
-# Añadir las nuevas entradas de log a la lista
-existing_log_entries.extend(log_entries)
+        # Actualizar user_message para la próxima iteración
+        user_message = response_content
 
-# Guardar la lista completa de entradas de log en un archivo JSON
-with open(filename, 'w') as file:
-    json.dump(existing_log_entries, file, indent=2, ensure_ascii=False)
+    # Guardar la lista completa de entradas de log en un archivo JSON
+    with open(filename, 'w') as file:
+        print(f"Guardando log en {filename}")
+        json.dump(log_entries, file, indent=2, ensure_ascii=False)
+
+    print(f"Iteración {i + 1} completada\n{'-'*50}")
+
+print("Proceso completado.")
