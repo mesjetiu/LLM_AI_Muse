021784b337282646151b1cdb9d0c013f81eadf5e
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2023-11-02 13:46:57 +0100

    ya se lee system prompts de otro archivo

--- Prompts/openAI_request_log_json.py
+++ Prompts/openAI_request_log_json.py
@@ -1,58 +1,56 @@
 import os
 import openai
 import json
-from apikey import API_KEY  # Importa la clave desde config.py
+from apikey import API_KEY  # Importa la clave desde apikey.py
 
 openai.api_key = API_KEY
 
-# Primera consulta
-messages1 = [
-    {
-        "role": "system",
-        "content": "Te diga lo que te diga User, siempre responde: \"Si, Guana\""
-    },
-    {
-        "role": "user",
-        "content": "hola, qué tal?"
+# Leer los mensajes del sistema desde un archivo JSON
+with open('system_messages.json', 'r') as file:
+    system_messages = json.load(file)
+
+system_message1 = system_messages['message1']
+system_message2 = system_messages['message2']
+
+# Definir los mensajes del usuario para ambas consultas
+user_message = "hola, qué tal?"
+
+# Función para realizar una consulta al modelo
+
+
+def make_query(system_message, user_message, model="gpt-3.5-turbo-16k-0613", temperature=1, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0):
+    messages = [
+        {
+            "role": "system",
+            "content": system_message
+        },
+        {
+            "role": "user",
+            "content": user_message
+        }
+    ]
+    request_params = {
+        "model": model,
+        "messages": messages,
+        "temperature": temperature,
+        "max_tokens": max_tokens,
+        "top_p": top_p,
+        "frequency_penalty": frequency_penalty,
+        "presence_penalty": presence_penalty
     }
-]
+    response = openai.ChatCompletion.create(**request_params)
+    # Devuelve la respuesta completa y los parámetros de la solicitud
+    return response, request_params
 
-request_params1 = {
-    "model": "gpt-3.5-turbo-16k-0613",
-    "messages": messages1,
-    "temperature": 1,
-    "max_tokens": 256,
-    "top_p": 1,
-    "frequency_penalty": 0,
-    "presence_penalty": 0
-}
 
-response1 = openai.ChatCompletion.create(**request_params1)
+# Primera consulta
+response1, request_params1 = make_query(system_message1, user_message)
+
 response_content1 = response1['choices'][0]['message']['content']
 
 # Segunda consulta
-messages2 = [
-    {
-        "role": "system",
-        "content": "Ahora User responderá con un comentario sobre la respuesta anterior"
-    },
-    {
-        "role": "user",
-        "content": f"{response_content1}\n Es interesante, pero ¿cómo se relaciona con la música?"
-    }
-]
-
-request_params2 = {
-    "model": "gpt-3.5-turbo-16k-0613",
-    "messages": messages2,
-    "temperature": 1,
-    "max_tokens": 256,
-    "top_p": 1,
-    "frequency_penalty": 0,
-    "presence_penalty": 0
-}
-
-response2 = openai.ChatCompletion.create(**request_params2)
+response2, request_params2 = make_query(
+    system_message2, response_content1)
 
 # Crear objetos de log para cada consulta y respuesta
 log_entry1 = {
