5d22842962bd0933ea96b863a16b18a61752a40d
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2023-11-02 16:43:17 +0100

    system y user prompts en archivos diferentes.

--- Prompts/openAI_request_log_json.py
+++ Prompts/openAI_request_log_json.py
@@ -14,13 +14,31 @@ with open('system_messages.json', 'r') as file:
 selected_group_key = "group2"  # o "group2" para usar el otro grupo
 system_messages = all_system_messages[selected_group_key]
 
-# Definir el mensaje del usuario inicial
-user_message = "hola, qué tal?"
+# Leer los mensajes del usuario desde un archivo JSON
+with open('user_messages.json', 'r') as file:
+    all_user_messages = json.load(file)
+
+# Seleccionar el mensaje del usuario que deseas usar
+selected_user_key = "message1"  # o "message2" o "message3" para usar otros mensajes
+user_message = all_user_messages[selected_user_key]
+
+# Definir cuántas veces ejecutar todo el script
+num_iterations = 2
+
+# Definir el modelo a utilizar
+model = "gpt-3.5-turbo-16k-0613"
+
+# Definir los parámetros de la consulta
+temperature = 1
+max_tokens = 256
+top_p = 1
+frequency_penalty = 0
+presence_penalty = 0
 
 # Función para realizar una consulta al modelo
 
 
-def make_query(system_message, user_message, model="gpt-3.5-turbo-16k-0613", temperature=1, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0):
+def make_query(system_message, user_message, model, temperature, max_tokens, top_p, frequency_penalty, presence_penalty):
     print(f"Enviando consulta: {system_message} / {user_message}")
     messages = [
         {
@@ -42,14 +60,14 @@ def make_query(system_message, user_message, model="gpt-3.5-turbo-16k-0613", tem
         "presence_penalty": presence_penalty
     }
     response = openai.ChatCompletion.create(**request_params)
-    print("Respuesta recibida")
+    print(
+        f"Respuesta recibida: {response['choices'][0]['message']['content']}")
     return response, request_params
 
+# Función para procesar una iteración completa
 
-# Definir cuántas veces ejecutar todo el script
-num_iterations = 2
 
-for i in range(num_iterations):
+def process_iteration(i, user_message):
     print(f"Iteración {i + 1} de {num_iterations}")
 
     # Obtener la fecha y hora actual para incluirlas en el nombre del archivo de log
@@ -61,7 +79,8 @@ for i in range(num_iterations):
 
     # Iterar a través de los mensajes del sistema y realizar una consulta para cada uno
     for step, system_message in enumerate(system_messages, start=1):
-        response, request_params = make_query(system_message, user_message)
+        response, request_params = make_query(
+            system_message, user_message, model, temperature, max_tokens, top_p, frequency_penalty, presence_penalty)
         response_content = response['choices'][0]['message']['content']
 
         # Crear un objeto de log para la consulta y respuesta actual
@@ -85,5 +104,10 @@ for i in range(num_iterations):
         json.dump(log_entries, file, indent=2, ensure_ascii=False)
 
     print(f"Iteración {i + 1} completada\n{'-'*50}")
+
+
+# Procesar todas las iteraciones
+for i in range(num_iterations):
+    process_iteration(i, user_message)
 
 print("Proceso completado.")
