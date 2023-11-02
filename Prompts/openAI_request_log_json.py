bf22806d4fbdb8e77f91a097e2eb0b6c7d68791a
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2023-11-02 14:04:22 +0100

    los prompts tienen un número indefinido de system prompts

--- Prompts/openAI_request_log_json.py
+++ Prompts/openAI_request_log_json.py
@@ -9,14 +9,11 @@ openai.api_key = API_KEY
 with open('system_messages.json', 'r') as file:
     all_system_messages = json.load(file)
 
-# Seleccionar el par de mensajes del sistema que deseas usar
-selected_pair_key = "pair2"  # o "pair2" para usar el otro par
-system_messages = all_system_messages[selected_pair_key]
+# Seleccionar el grupo de mensajes del sistema que deseas usar
+selected_group_key = "group2"  # o "group2" para usar el otro grupo
+system_messages = all_system_messages[selected_group_key]
 
-system_message1 = system_messages['message1']
-system_message2 = system_messages['message2']
-
-# Definir los mensajes del usuario para ambas consultas
+# Definir el mensaje del usuario inicial
 user_message = "hola, qué tal?"
 
 # Función para realizar una consulta al modelo
@@ -47,25 +44,25 @@ def make_query(system_message, user_message, model="gpt-3.5-turbo-16k-0613", tem
     return response, request_params
 
 
-# Primera consulta
-response1, request_params1 = make_query(system_message1, user_message)
+# Inicializar una lista vacía para almacenar las entradas de log
+log_entries = []
 
-response_content1 = response1['choices'][0]['message']['content']
+# Iterar a través de los mensajes del sistema y realizar una consulta para cada uno
+for system_message in system_messages:
+    response, request_params = make_query(system_message, user_message)
+    response_content = response['choices'][0]['message']['content']
 
-# Segunda consulta
-response2, request_params2 = make_query(
-    system_message2, response_content1)
+    # Crear un objeto de log para la consulta y respuesta actual
+    log_entry = {
+        "request": request_params,
+        "response": response['choices'][0]
+    }
 
-# Crear objetos de log para cada consulta y respuesta
-log_entry1 = {
-    "request": request_params1,
-    "response": response1['choices'][0]
-}
+    # Añadir la entrada de log a la lista
+    log_entries.append(log_entry)
 
-log_entry2 = {
-    "request": request_params2,
-    "response": response2['choices'][0]
-}
+    # Actualizar user_message para la próxima iteración
+    user_message = response_content
 
 # Nombre del archivo en el que se guardarán los registros
 filename = "chat_log.json"
@@ -73,13 +70,13 @@ filename = "chat_log.json"
 # Leer el archivo existente y deserializar el contenido a una lista
 try:
     with open(filename, 'r') as file:
-        log_entries = json.load(file)
+        existing_log_entries = json.load(file)
 except FileNotFoundError:
-    log_entries = []  # Si el archivo no existe, iniciar una lista vacía
+    existing_log_entries = []  # Si el archivo no existe, iniciar una lista vacía
 
 # Añadir las nuevas entradas de log a la lista
-log_entries.extend([log_entry1, log_entry2])
+existing_log_entries.extend(log_entries)
 
 # Guardar la lista completa de entradas de log en un archivo JSON
 with open(filename, 'w') as file:
-    json.dump(log_entries, file, indent=2, ensure_ascii=False)
+    json.dump(existing_log_entries, file, indent=2, ensure_ascii=False)
