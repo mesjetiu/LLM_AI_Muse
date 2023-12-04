526d0047ef116ea9acc9b342c2fe634ec2182afc
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2023-12-04 20:54:21 +0100

    Se puede enviar parametro nulo a los comandos

--- Experimentos/Tidal Cycles/python/LiveCoding_interactivo_API/command_functions.py
+++ Experimentos/Tidal Cycles/python/LiveCoding_interactivo_API/command_functions.py
@@ -3,7 +3,7 @@ from program_state import estado_programa
 from config_manager import config
 
 
-def set_api_on_off_command(new_state):
+def set_api_on_off_command(new_state=None):
     if new_state == "on":
         config["api_enabled"] = True
         print("API activada.")
@@ -41,45 +41,45 @@ def set_api_on_off_command(new_state):
 #         print("Comando no reconocido. Introduce 'restart ghci' o 'restart sclang'.")
 
 
-def set_model_command(new_model):
+def set_model_command(new_model=None):
     config["model"] = new_model.strip()
     print(f"Modelo cambiado a {config['model']}.")
 
 
-def set_temperature_command(new_temperature):
+def set_temperature_command(new_temperature=None):
     config["temperature"] = float(new_temperature.strip())
     print(f"Temperatura cambiada a {config['temperature']}.")
 
 
-def set_max_tokens_command(new_max_tokens):
+def set_max_tokens_command(new_max_tokens=None):
     config["max_tokens"] = int(new_max_tokens.strip())
     print(f"Max tokens cambiado a {config['max_tokens']}.")
 
 
-def set_top_p_command(new_top_p):
+def set_top_p_command(new_top_p=None):
     config["top_p"] = float(new_top_p.strip())
     print(f"Top P cambiado a {config['top_p']}.")
 
 
-def set_frequency_penalty_command(new_frequency_penalty):
+def set_frequency_penalty_command(new_frequency_penalty=None):
     config["frequency_penalty"] = float(new_frequency_penalty.strip())
     print(
         f"Penalización de frecuencia cambiada a {config['frequency_penalty']}.")
 
 
-def set_presence_penalty_command(new_presence_penalty):
+def set_presence_penalty_command(new_presence_penalty=None):
     config["presence_penalty"] = float(new_presence_penalty.strip())
     print(
         f"Penalización de presencia cambiada a {config['presence_penalty']}.")
 
 
-def set_wait_time_before_api_command(new_wait_time):
+def set_wait_time_before_api_command(new_wait_time=None):
     config["wait_time_before_api"] = int(new_wait_time.strip())
     print(
         f"Tiempo de espera antes de la API cambiado a {config['wait_time_before_api']} segundos.")
 
 
-def set_wait_time_after_api_command(new_wait_time):
+def set_wait_time_after_api_command(new_wait_time=None):
     config["wait_time_after_api"] = int(new_wait_time.strip())
     print(
         f"Tiempo de espera después de la API cambiado a {config['wait_time_after_api']} segundos.")
